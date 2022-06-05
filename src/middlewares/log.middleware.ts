03d7f0f55c3339f3700aebf888440d17428e2953
Kennys Ng <kennysng@hotmail.com.hk>
2022-06-06 07:55:34 +0800

    fix http logger tag

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -3,25 +3,26 @@ import type { NextFunction, Request, Response } from 'express';
 
 import { Injectable, Logger } from '@nestjs/common';
 
-const logger = new Logger('Request', { timestamp: true });
+const reqLogger = new Logger('Request', { timestamp: true });
+const resLogger = new Logger('Response', { timestamp: true });
 
 @Injectable()
 export class LogMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl: url } = request;
     const contentLength = request.header('content-length') || 0;
-    logger.log(
+    reqLogger.log(
       `${method} ${url} from ip=${ip} Start (content-length = ${contentLength})`,
     );
     const authorization = request.header('authorization');
     const deviceToken = request.header('x-device-token');
-    if (authorization) logger.log(`Authorization: ${authorization}`);
-    if (deviceToken) logger.log(`X-Device-Token: ${deviceToken}`);
-    if (contentLength) logger.log(`body = ${JSON.stringify(request.body)}`);
+    if (authorization) reqLogger.log(`Authorization: ${authorization}`);
+    if (deviceToken) reqLogger.log(`X-Device-Token: ${deviceToken}`);
+    if (contentLength) reqLogger.log(`body = ${JSON.stringify(request.body)}`);
     response.on('close', () => {
       const { statusCode } = response;
-      logger.log(`${method} ${url} End with code=${statusCode}`);
-      logger.log(
+      resLogger.log(`${method} ${url} End with code=${statusCode}`);
+      resLogger.log(
         `content-length = ${response.getHeader('content-length') || 0}`,
       );
     });
