86b108705f6f79bea14e434cb2d9a23713babd46
Kennys Ng <kennysng@hotmail.com.hk>
2022-08-14 10:03:03 +0800

    feat: queue server

--- index.ts
+++ index.ts
@@ -1,35 +1,80 @@
-import Queue, { Job } from 'bee-queue';
-import fastify from 'fastify';
 import compression from '@fastify/compress';
 import helmet from '@fastify/helmet';
-import yaml from 'js-yaml';
+import Queue = require('bee-queue');
+import fastify from 'fastify';
 import { readFile } from 'fs/promises';
+import { NotFound, RequestTimeout } from 'http-errors';
+import yaml = require('js-yaml');
+import minimist = require('minimist');
 import { resolve } from 'path';
 import { IConfig, IResult } from 'interface';
-import { v4 } from 'uuid';
+import { match } from 'node-match-path';
+import { URL } from 'url';
+
+const argv = minimist(process.argv.slice(2));
+
+function wait<T>(queue: Queue, job: Queue.Job<T>, timeout: number) {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(async () => {
+      await queue.removeJob(job.id);
+      reject(new RequestTimeout());
+    }, timeout);
+    job.on('succeeded', (result: IResult) => {
+      clearTimeout(timer);
+      resolve(result);
+    });
+    job.on('failed', (e) => reject(e));
+  });
+}
 
 async function main() {
-  if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
+  const NODE_ENV = (process.env.NODE_ENV =
+    argv.env || argv.E || process.env.NODE_ENV || 'development');
   const content = await readFile(
-    resolve('configs', `${process.env.NODE_ENV}.yaml`),
+    resolve('configs', `${NODE_ENV}.yaml`),
     'utf8',
   );
-  const config: IConfig = yaml.load(content);
+  const config = yaml.load(content) as IConfig;
+  const port = config.port || 8080;
+  const redisConfig = config.redis || {};
 
-  const queue = new Queue('');  // TODO
+  const queues: Record<string, Queue> = {};
 
   const app = fastify({ logger: true });
   app.register(helmet);
   app.register(compression);
-  app.all('*', async (request, reply) => {
-    const job = queue.createJob({
-      // TODO
-    });
-    await job.save();
-    job.on('succeeded', (result: IResult) => {
-      reply.send(result);
-    });
+  app.all('*', async (request) => {
+    const url = new URL(request.url, `http://localhost:${port}`);
+    for (const [key, value] of Object.entries(config.queues)) {
+      const { matches } = match(value, url.pathname);
+      if (matches) {
+        let queue = queues[key];
+        if (!queue) {
+          queue = queues[key] = new Queue(key, {
+            getEvents: false,
+            isWorker: false,
+            redis: redisConfig,
+          });
+        }
+        queue.on('ready', () => request.log.info(`Queue '${key}' is ready`));
+        const job = await queue
+          .createJob({
+            method: request.method,
+            url: request.url,
+            params: request.query,
+          })
+          .save();
+        return await wait(queue, job, config.timeout || 30 * 1000);
+      }
+    }
+    throw new NotFound();
   });
+
+  process.on('beforeExit', async () => {
+    await Promise.allSettled(Object.values(queues).map((q) => q.close()));
+  });
+
+  await app.listen({ port });
 }
 
 main();
--- interface.ts
+++ interface.ts
@@ -1,7 +1,16 @@
 import { HttpStatus } from 'http-status';
 
 export interface IConfig {
-  // TODO
+  port?: number;
+  timeout?: number;
+  queues: Record<string, string>;
+  redis?: {
+    secure?: boolean;
+    host?: string;
+    port?: number;
+    username?: string;
+    password?: string;
+  };
 }
 
 export interface IResult {
