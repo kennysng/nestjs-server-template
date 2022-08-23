9e6c4d638c6d7e2e4b752c03245c94aa53113b80
Kennys Ng (ESD) - EXT <ext-esdkeng@hktdc.org>
2022-08-23 12:20:29 +0800

    demo: LastModified

--- src/queue/default.ts
+++ src/queue/default.ts
@@ -1,11 +1,25 @@
 import httpStatus = require('http-status');
-import { Queue, Path } from '../decorator';
+import { Queue, Path, LastModified } from '../decorator';
+
+let date: Date;
 
 @Queue()
 export default class DefaultQueue {
   @Path('HEALTH')
   healthCheck() {
     return { statusCode: httpStatus.OK };
+  }
+
+  @LastModified(() => {
+    if (!date) date = new Date();
+    return date;
+  })
+  @Path('GET', 'cached')
+  cachedPath() {
+    return {
+      statusCode: httpStatus.OK,
+      result: 'Hello World!',
+    };
   }
 
   @Path('ALL')
